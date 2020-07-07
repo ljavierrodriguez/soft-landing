1f8c17a85235b2d099411cba0056cbcbf2ad58b0
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-07-07 01:13:26 -0400

    Config Database Tables

---
+++ app/Http/Controllers/TypeFeatureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TypeFeature;
+use Illuminate\Http\Request;
+
+class TypeFeatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TypeFeature  $typeFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypeFeature $typeFeature)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TypeFeature  $typeFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TypeFeature $typeFeature)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TypeFeature  $typeFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypeFeature $typeFeature)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TypeFeature  $typeFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypeFeature $typeFeature)
+    {
+        //
+    }
+}
